0736a5a6fd2a2202fabaaae4944187b6793f3628
Ajay4145 <ajaytamang4145@gmail.com>
2026-03-13 20:15:18 +0545

    [+] transaction schema

---
+++ src/models/transaction.model.js
@@ -0,0 +1,38 @@
+import { connectDB } from "../config/db.js";
+
+export const createAccountTable = async () => {
+  try {
+    const connection = await connectDB();
+    await connection.execute(`
+            CREATE TABLE IF NOT EXISTS transactions (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+
+                from_account INT NOT NULL,
+                to_account INT NOT NULL,
+
+                status ENUM('PENDING','COMPLETED','FAILED','REVERSED') DEFAULT 'PENDING',
+                amount DECIMAL(12,2) NOT NULL CHECK (amount >= 0),
+
+                idempotency_key VARCHAR(255) NOT NULL UNIQUE,
+
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
+                    ON UPDATE CURRENT_TIMESTAMP,
+
+                INDEX idx_from_account (from_account),
+                INDEX idx_to_account (to_account),
+                INDEX idx_idempotency_key (idempotency_key),
+
+                
+                FOREIGN KEY (from_account) REFERENCES accounts(id) ON DELETE RESTRICT,
+                FOREIGN KEY (to_account) REFERENCES accounts(id) ON DELETE RESTRICT
+            )
+            `);
+    console.log(`transactions Table created successfully`);
+  } catch (error) {
+    console.log(`Error to create a transactions Table`, error);
+    process.exit(1);
+  }
+};
+
+await createAccountTable();
